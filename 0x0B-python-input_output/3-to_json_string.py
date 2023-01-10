1246ff512668554da21d0277a74cb2250db0c58a
Ezby02 <111150589+Ezby02@users.noreply.github.com>
2023-01-10 19:02:00 +0300

    Update 3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,1 +1,16 @@
-ii
+#!/usr/bin/python3
+''' function that returns the JSON representation of an object (string)
+'''
+
+import json
+
+
+def to_json_string(my_obj):
+    ''' module to_json_strin
+     returns JSON representation
+    '''
+    return json.dumps(my_obj)
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
