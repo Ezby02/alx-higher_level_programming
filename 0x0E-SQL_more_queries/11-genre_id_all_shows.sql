fecc6dc0165f4ad4d215b074977bfe2202e3992d
Ezby02 <111150589+Ezby02@users.noreply.github.com>
2023-02-17 01:20:37 +0300

    Update 11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
---lists all shows on hbtn_0d_tvshows
+-- lists all shows contained in the database hbtn_0d_tvshows
+-- lists all rows of tables in a database
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM hbtn_0d_tvshows
-LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_iid
+FROM tv_shows LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
