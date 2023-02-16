3a5b73a97957cb74f98dc52f23ac19a650a23473
Ezby02 <esperancemwanza@gmail.com>
2023-02-16 14:04:26 -0800

    2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- creates database hbtn_0d_2 & user user_0d_2 who has SELECT privileges only
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_od_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO  'user_od_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
