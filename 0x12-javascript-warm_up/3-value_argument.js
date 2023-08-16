d0f33009a128532bcce43f8081b3e08e9059c4a6
Ezby02 <111150589+Ezby02@users.noreply.github.com>
2023-08-16 18:58:14 +0300

    Update 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,2 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-
-if (process.argv[2] === undefined) {
-  console.log('No argument');
-} else {
-  console.log(process.argv[2]);
-}
+console.log(process.argv[2] ? process.argv[2] : 'No argument');
